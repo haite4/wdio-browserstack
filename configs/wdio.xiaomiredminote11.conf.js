f524ccda42c10e67ae3c73649b3306528f72033c
Stanislav Stankov <ugrayvcsgo137@gmail.com>
2024-08-30 13:53:18 +0100

    Update yml

--- configs/wdio.xiaomiredminote11.conf.js
+++ configs/wdio.xiaomiredminote11.conf.js
@@ -9,7 +9,7 @@ export const config = {
     [
       'browserstack',
       {
-        app:  "browserstack_test\Android-NativeDemoApp-0.4.0.apk",
+        app:  "C:/Users/ugray/Desktop/webdriverio-browserstack/browserstack_test/Android-NativeDemoApp-0.4.0.apk",
         browserstackLocal: true
       },
     ]
